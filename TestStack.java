abf035bc7fee65f93e5f3f80332083d5eefe6e38
Anushanallap <anusha.n468@gmail.com>
2021-12-21 16:43:27 +0530

    add teststack file

---
+++ TestStack.java
@@ -0,0 +1,24 @@
+package StacknQueue;
+
+public class TestStack {
+	
+	public static void main(String[] args)
+	{
+		Stack Stack = new Stack();
+		Stack.insert(56);
+		Stack.insert(30);
+        Stack.insert(70);
+        Stack.display();
+        Stack.peak();
+       // Stack.delete();
+        //Stack.delete();
+        //Stack.delete();
+        Stack.display();
+
+
+    }
+		
+		
+	}
+
+
